c84b6be7904ee91e448c497524592ba7817c3134
saran KS <sks@presidio.com>
2023-04-04 19:26:47 +0530

    update host

--- Enotes_Management_System/settings.py
+++ Enotes_Management_System/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h=#p#1$e9vnd8_ke*nzr+=0(o7b49t2=3yf@tr#(f-7tyn%u5-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
