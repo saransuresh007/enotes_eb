fcd5b331086451ae03993ac53e979fb9208d4ee5
saran KS <sks@presidio.com>
2023-04-12 15:00:33 +0530

    pylint check10

--- Enotes_Management_System/wsgi.py
+++ Enotes_Management_System/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for enotes_management_system project.
+WSGI config for Enotes_Management_System project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'enotes_management_system.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Enotes_Management_System.settings')
 
 application = get_wsgi_application()
